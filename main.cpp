7a95ec7f61039ad4a0157f2021601d717a41f19e
Joseph Pacia <jpaci001@ucr.edu>
2016-11-01 20:57:36 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+void parse(char *line, char **argv)
+{
+    while(*line != '\0')
+    {
+        // else if (*line == ';' || *line == '|' || *line == '&')
+        // {
+        //   line++; 
+        // }
+        while (*line == ' ' || *line == '\t' || *line == '\n')
+        {
+            
+            *line++ = '\0';
+        }
+        *argv++ = line;
+        while(*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+        {
+             if (*line == '#')
+             {
+                 line--;
+                 return;
+             }
+            line++;
+        }
+    }
+    *argv = '\0';
+}
+
+void execute(char **argv)
+{
+    pid_t pid;
+    int status;
+    if ((pid = fork()) < 0)
+    {
+        perror("ERROR: ");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        if (execvp(*argv, argv) < 0)
+        {
+            perror("ERROR: ");
+            exit(1);
+        }
+    }
+    else
+    {
+        while(wait(&status) != pid);
+    }
+}
+
+int main()
+{
+    cout << "This is our command shell." << endl;
+    cout << "$ ";
+    char input[1024];
+    char *argv[64];
+    cin.getline(input, 1024);
+    parse(input, argv);
+     
+    for (int i = 0; argv[i] != NULL; i++)
+    {
+        cout << argv[i] << endl;
+    }
+    //  char *token = strtok(input, " ");
+    //  while (token != NULL)
+    //  {
+    //      cout << token << endl;
+    //       token = strtok(NULL, " ");
+    //  }
+    return 0;
+}
