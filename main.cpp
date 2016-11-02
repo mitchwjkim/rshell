333a1151a73f4b069672eee3f76177130ef8b19a
Joseph Pacia <jpaci001@ucr.edu>
2016-11-02 06:49:55 +0000

    almost done with parsing sort of

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ using namespace std;
 void parse(string letter, char **argv, vector<char> &con)
 {
     vector<string> cmd;
-    //vector<char> con;
     bool end_flag = false;
     bool letter_flag = false;
     
@@ -63,20 +62,20 @@ void parse(string letter, char **argv, vector<char> &con)
     }
     
     //Print the vectors
-    for(int j = 0; j < cmd.size(); j++)
-    {
-        cout << cmd.at(j) << endl;
-        if(j < con.size())
-            cout << con.at(j) << endl;
-        else
-        {
-            // Nothing
-        }
-    }
+    // for(int j = 0; j < cmd.size(); j++)
+    // {
+    //     cout << cmd.at(j) << endl;
+    //     if(j < con.size())
+    //         cout << con.at(j) << endl;
+    //     else
+    //     {
+    //         // Nothing
+    //     }
+    // }
     
     // Parse the Parse ( Vector<string> cmd into Char * Array into Vector of the Char * Array )
     
-    vector<char*> vec_cmd;
+    vector<char**> vec_cmd;
     bool cmd_flag = false;                      // While loop flag
     for (int k = 0; k < cmd.size(); k++)
     {
@@ -94,84 +93,106 @@ void parse(string letter, char **argv, vector<char> &con)
                 }
                 else
                 {
+                    //temp_hold += '\0';                      // Add Null Character
                     temp.push_back(temp_hold);
                     temp_hold = '\0';
                 }
             }
-            temp.push_back(temp_hold);                      // Add last string
-            temp_hold = '\0';
+            //temp_hold += '\0';                              // Add Null Character on last string
+            temp.push_back(temp_hold);                        // Add last string
+            temp.push_back('\0');                             // Have the last index be '\0'
+            //temp_hold = '\0';
             
             cmd_flag = true;
         }
         
-        for (int i = 0; i < temp.size(); i++)
-        {
-            argv = new char * [temp.size()];
-            argv[i] = new char[temp.at(i)];
-            strcpy (argv[i], temp.at(i));
-        }
-      //  vec_cmd.push_back(argv);
-    }
+         for (int i = 0; i < temp.size(); i++)
+         {
+             argv = new char * [temp.size()];
+             char *holder = temp.at(i);
+             int count = 0;
+             while (holder[count] != '\0')
+             {
+                 count++;
+             }
+             argv[i] = new char[count];
+             strcpy (argv[i], temp.at(i));
+         }
+         cmd_flag = false;
+         vec_cmd.push_back(argv);
+     }
+    
+     cout << endl << "vec_cmd Data:" << endl;
+     for(int i = 0; i < vec_cmd.size(); i++)
+     {
+         cout << vec_cmd[i] << endl;
+     }
 }
 
 
-// void execute(char **argv, vector<char> &con)
-// {
-//     int i = 0;
-//     while (true)
-//     {
-//         pid_t pid;
-//         int status;
-//         if ((pid = fork()) < 0)
-//         {
-//             perror("ERROR: ");
-//             if(con[i] == '|' || con[i] == ';')
-//             {
-//                 i++;
-//                 continue;
-//             }
-//             else
-//             {
-//                 exit(1);
-//             }
-//         }
-//         else if (pid == 0)
-//         {
-//             if (execvp(*argv, argv) < 0)
-//             {
-//                 perror("ERROR: ");
-//                 if(con[i] == '|' || con[i] == ';')
-//                 {
-//                     i++;
-//                     continue;
-//                 }
-//                 else
-//                 {
-//                     exit(1);
-//                 }
-//             }
-//         }
-//         else
-//         {
-//             while(wait(&status) != pid);
-//         }
+void execute(char *argv[], vector<char> &con)
+{
+    int i = 0;
+    while (true)
+    {
+        pid_t pid;
+        int status;
+        if ((pid = fork()) < 0)
+        {
+            perror("ERROR: ");
+            if(con[i] == '|' || con[i] == ';')
+            {
+                i++;
+                continue;
+            }
+            else
+            {
+                exit(1);
+            }
+        }
+        else if (pid == 0)
+        {
+            if (execvp(*argv, argv) < 0)
+            {
+                perror("ERROR: ");
+                if(con[i] == '|' || con[i] == ';')
+                {
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    exit(1);
+                }
+            }
+        }
+        else
+        {
+            while(wait(&status) != pid);
+        }
         
         
-//     }
-// }
+    }
+}
+
+struct command
+{
+    char *c[64];
+};
 
 int main()
 {
     vector<char> con;
+    vector<command>cmd;
     cout << "This is our command shell." << endl;
     cout << "$ ";
-    //char input[1024];
-    string input;
-    char **argv;
-    //cin.getline(input, 1024);
-    getline(cin, input);
-    parse(input, argv, con);
-    string test = "ls -a";
+    char input[1024];
+    //string input;
+    char *argv[64];
+    cin.getline(input, 1024);
+   // getline(cin, input);
+    //parse(input, argv, con);
+    //string test = "ls -a";
     
     //test(test);
     
@@ -180,12 +201,49 @@ int main()
     //{
     //    cout << argv[i] << endl;
     //}
-     
-    //  char *token = strtok(input, " ");
-    //  while (token != NULL)
-    //  {
-    //      cout << token << endl;
-    //       token = strtok(NULL, " ");
-    //  }
+      int i = 0;
+      char *token = strtok(input, " ");
+      char *temp;
+      bool vflag = false;
+     while (token != NULL)
+     {
+         argv[i] = token;
+         temp = argv[i];
+         for (int j = 0; temp[j] != '\0'; j++)
+         {
+             if(temp[j] == ';')
+             {
+                 vflag = true;
+                 con.push_back(temp[j]);
+                 temp[j] = '\0';
+             }
+             if(temp[j] == '&' || temp[j] == '|')
+             {
+                 vflag = true;
+                 con.push_back(temp[j]);
+                 temp[j] = temp[j+1] = '\0';
+                 j++;
+             }
+         }
+         //cout << token << endl;
+          argv[i] = temp;
+          if (vflag)
+          {
+              
+            //   command co;
+            //   int ccount = 0;
+            //   while(argv[i] != '\0')
+            //   {
+            //       co.c[ccount] = argv[i]; 
+            //       ccount++;
+            //   }
+             
+            //   cmd.push_back(co);
+             
+          }
+          token = strtok(NULL, " ");
+          i++;
+        vflag = false;
+     }
     return 0;
 }
